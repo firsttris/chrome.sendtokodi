cbb4a0a3626ab52b1385a32e7b0b8119f0ccbe26
firsttris <tristanteufel@googlemail.com>
2017-09-22 23:49:53 +0200

    feat(components): migration to react

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,26 +4,77 @@ class Settings extends Component {
   constructor() {
     super();
     this.state = {
-      link: ''
+      ip: '',
+      port: '',
+      login: '',
+      pw: '',
+      error: '',
+      good: false
     };
   }
 
-  handleChange(event) {
-    this.setState({ link: event.target.value });
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
-  componentWillMount() {}
+  componentWillMount() {
+    chrome.storage.sync.get('settings', result => {
+      if (Object.values(result).length) {
+        const s = result.settings;
+        this.setState({ ip: s.ip, port: s.port, login: s.login, pw: s.pw });
+      }
+    });
+  }
+
+  save() {
+    chrome.storage.sync.set({ settings: this.state });
+  }
+
+  testConnection() {
+    fetch('http://' + this.state.ip + ':' + this.state.port + '/jsonrpc', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + btoa(this.state.pw + ':' + this.state.login)
+      },
+      body: JSON.stringify({
+        method: 'Addons.GetAddonDetails',
+        id: 0,
+        jsonrpc: '2.0',
+        params: { addonid: 'plugin.video.sendtokodi' }
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        this.setState({ good: false, error: 'Unauthorized' });
+      })
+      .then(
+        json =>
+          json.result.addon.addonid === 'plugin.video.sendtokodi'
+            ? this.setState({ good: true })
+            : this.setState({ good: false })
+      )
+      .catch(error => {
+        this.setState(error);
+      });
+  }
 
   render() {
     return (
       <div className="container mt-3" style={{ width: '500px' }}>
         <div className="form-group">
-          <label htmlFor="ipadress">IP Adresse</label>
+          <label htmlFor="ip">IP Adresse</label>
           <input
             className="form-control"
             type="text"
+            name="ip"
             placeholder="127.0.0.1"
-            id="ipadress"
+            id="ip"
+            value={this.state.ip}
+            onChange={e => this.handleInputChange(e)}
           />
         </div>
         <div className="form-group">
@@ -31,8 +82,11 @@ class Settings extends Component {
           <input
             className="form-control"
             type="text"
+            name="port"
             placeholder="8080"
             id="port"
+            value={this.state.port}
+            onChange={e => this.handleInputChange(e)}
           />
         </div>
         <div className="form-group">
@@ -40,26 +94,37 @@ class Settings extends Component {
           <input
             className="form-control"
             type="text"
+            name="login"
             placeholder="kodi"
             id="login"
+            value={this.state.login}
+            onChange={e => this.handleInputChange(e)}
           />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="pw">Password</label>
           <input
             className="form-control"
             type="password"
+            name="pw"
             placeholder="kodi"
-            id="password"
+            id="pw"
+            value={this.state.pw}
+            onChange={e => this.handleInputChange(e)}
           />
         </div>
         <div className="form-group">
-          <button type="submit" className="btn btn-secondary">
+          <button className="btn btn-secondary" onClick={() => this.save()}>
             Save
           </button>{' '}
-          <button type="submit" className="btn btn-secondary">
+          <button
+            className="btn btn-secondary"
+            onClick={() => this.testConnection()}
+          >
             Test
-          </button>
+          </button>{' '}
+          {this.state.good ? <span style={{ color: 'green' }}>Good</span> : ''}
+          <p>{this.state.error}</p>
         </div>
       </div>
     );
