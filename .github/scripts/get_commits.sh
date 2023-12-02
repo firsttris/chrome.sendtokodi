171018c7ceb1173e34842ec874b8c31f9a0802a6
Tristan Teufel <tristanteufel@googlemail.com>
2023-12-02 09:45:08 +0100

    extract get commits since last release to bash

--- .github/scripts/get_commits.sh
+++ .github/scripts/get_commits.sh
@@ -9,6 +9,12 @@ sha=$(git rev-list -n 1 $tag)
 # Get the commits since the last release
 list=$(git log --pretty=format:"%h %s" $sha..HEAD)
 
-echo "tag: $tag"
-echo "sha: $sha"
-echo "commits: $list"
+# Write the output to an environment variable
+if [ -n "$GITHUB_ENV" ]; then
+    echo "COMMIT_LIST=$list" >> $GITHUB_ENV
+else
+    export COMMIT_LIST=$list
+fi
+
+# Log the value of the environment variable
+echo $COMMIT_LIST
