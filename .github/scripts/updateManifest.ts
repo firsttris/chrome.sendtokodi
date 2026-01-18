fe4f33f4eebcdbf64620fe8d7582ac7c8d216dd4
Tristan Teufel <tristanteufel@googlemail.com>
2026-01-18 13:15:29 +0100

    update ci

--- .github/scripts/updateManifest.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const manifestPath = path.resolve(__dirname, './../../dist/manifest.json');
-const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
-manifest['browser_specific_settings'] = { 'gecko': { 'id': 'sendtokodi@firsttris.github.io' } };
-fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
